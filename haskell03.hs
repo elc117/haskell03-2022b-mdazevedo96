91db9fc8ed58147e7b5bd0d1d0289cf87d77a119
mdazevedo96 <42869269+mdazevedo96@users.noreply.github.com>
2023-01-31 16:18:53 -0300

    Update haskell03.hs

--- haskell03.hs
+++ haskell03.hs
@@ -10,10 +10,45 @@ multN'  x list = map (*x) list
 applyExpr :: [Int] -> [Int]
 applyExpr list = [3*x+2 | x <- list]
 
---addSuffix :: String -> [String] -> [String]
---addSuffix suf list = [x| x <- suf++ list]
+add10toall :: [Int] -> [Int]
+add10toall list = [x+10 | x <- list]
 
---selectgt5 :: [Int] -> [Int]
---selectgt5 list = [ x > 5 | x <- list]
+multN :: Int -> [Int] -> [Int]
+multN x list = [x*y|y <- list]
 
+multN' :: Int -> [Int] -> [Int]
+multN'  x list = map (*x) list
+
+applyExpr :: [Int] -> [Int]
+applyExpr list = [3*x+2 | x <- list]
+
+addSuffix :: String -> [String] -> [String]
+addSuffix suffix list = [x ++ suffix | x <- list]
+
+selectgt5 :: [Int] -> [Int]
+selectgt5 list = [x | x <- list, x > 5]
+
+sumOdds :: [Int] -> Int
+sumOdds som = foldl (+) 0 [x | x <- som, odd x]
+
+sumOdds' :: [Int] -> Int
+sumOdds' som = foldl (+) 0 (filter (\x -> odd x) som)
+
+selectExpr :: [Int] -> [Int]
+selectExpr list = [x | x <- list, even x , x >= 20 , x <= 50]
+
+countShorts :: [String] -> Int
+countShorts list = length [x | x <- list, length x < 5]
+
+calcExpr :: [Float] -> [Float]
+calcExpr list = [x^2/2 | x <- list, (x^2/2) > 10]
+
+trSpaces :: String -> String
+trSpaces str = [if x == ' ' then '-' else x | x <- str]
+
+selectSnd :: [(Int,Int)] -> [Int]
+selectSnd tp = [snd t | t <- tp]
+
+dotProd :: [Int] -> [Int] -> Int
+dotProd lista lista2 = sum [x*y | (x,y) <- zip lista lista2]
 
